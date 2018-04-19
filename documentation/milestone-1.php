de7b25419cf701361419ea8a9643b8df9888ab79
G Cordova <gcordovaone@gmail.com>
2018-04-18 19:44:26 -0600

    set html layout

---
+++ documentation/milestone-1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>pwpproject</title>
+	</head>
+	<body>
+		<h1>Purpose, Audience, Goal</h1>
+		<div>
+			<h2>Purpose</h2>
+		</div>
+		<div>
+			<h2>Audience</h2>
+		</div>
+		<div>
+			<h2>Audience</h2>
+		</div>
+	</body>
+</html>
