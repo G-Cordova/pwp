6fef4d233e7528aae43c96add7a016ef3b6fa86c
G Cordova <gcordovaone@gmail.com>
2018-04-18 19:47:39 -0600

    added purpose statement

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -8,6 +8,9 @@
 		<h1>Purpose, Audience, Goal</h1>
 		<div>
 			<h2>Purpose</h2>
+			<p>
+				The purpose of my Personal Website Project is to have a platform that establishes my online presence as a web developer.
+			</p>
 		</div>
 		<div>
 			<h2>Audience</h2>
