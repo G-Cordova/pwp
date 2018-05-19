ad4fb19d5cb9d5d25417752100da916241fe236e
G Cordova <gcordovaone@gmail.com>
2018-05-19 16:22:10 -0600

    created nav bar and first container

--- index.php
+++ index.php
@@ -13,10 +13,34 @@
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 
 		<title>G-Cordova-pwp</title>
-
 	</head>
 
 	<body>
 
+		<div>
+			<nav class="container navbar">
+			<a class="navbar-brand"></a>
+			<a href="https://github.com/">GitHub</a>
+			<a href="https://google.com/">Email</a>
+			<a href="https://www.linkedin.com/"">Linkedin</a>
+			<a href="https://twitter.com/"">Twitter</a>
+			</nav>
+		</div>
+		<div class="container">
+			<div class="container-fluid mt-6">
+				<div class="jumbotron container">
+					<h1 class="display-3">Kickstart your business!</h1>
+					<p class="lead">This is a simple hero unit, a simple jumbotron-style component for
+						calling extra attention to featured content or information.</p>
+					<hr class="my-2">
+					<p>It uses utility classes for typography and
+						spacing to space content out within the larger container.</p>
+					<p class="lead">
+						<a class="btn btn-primary btn-lg" href="#!" role="button">Some action</a>
+					</p>
+				</div>
+			</div>
 	</body>
 </html>
+
+
