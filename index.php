d0ce2e048fbb3a20a080a5c84d7391c843901aad
G Cordova <gcordovaone@gmail.com>
2018-05-20 18:39:57 -0600

    added top margin to top of container, and place holders for text descriptions

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 			<div class="full-bg-img">
 			</div>
 
-		<div class="row-fluid mt-3">
+		<div class="row-fluid mt-9">
 			<div class="container-fluid">
 				<div class="jumbotron container">
 					<h1 class="display-3">Kick-start your business!</h1>
@@ -51,24 +51,25 @@
 		</div>
 		</div>
 
-	<div class="container marketing">
+	<div class="container marketing mt-3">
 		<div class="row">
 			<div class="col-lg-4 text-center">
 				<img class="img-thumbnail rounded-circle d-flex align-center rounded mx-auto" src="rawpixel-550994-unsplash.jpg" alt="">
 				<div class="card">
-					Maintenance - If you already have a website that you are seeking assistance or various maintence services on the site, we do provide maintenece service varying based on each specific case.
+					Digital Marketing & Social Media
+					Lucas ipsum dolor sit amet malastare sesswenna bane ysanne cb-99 nahdar keyan iblis hutt anzati. Qui-gonn hutt myneyrsh theelin darth lyn omwati offee. Kasan sanyassan max tharen veila. Neimoidian kit zuckuss qui-gon kaleesh oswaft bajic binks felucia. Annoo shado su thisspias d8 bria tyber givin needa. Gossam sluis evazan feeorin. Yarael corran sulorine talortai var nautolan kamino. Tharen bib salacious cassio bel yavin shadda bossk. Abregado muzzer borvo isard lobot. Ubb crynyd tof sifo-dyas shado arkanian sanyassan.
 				</div>
 			</div>
 			<div class="col-lg-4 text-center">
 				<img class="img-thumbnail rounded-circle d-flex align-center rounded mx-auto" src="rawpixel-649916-unsplash (1).jpg" alt="">
 				<div class="card">
-					Front-End & Back-End Development - We do provide services in both.
+					Maintenance - If you already have a website that you are seeking assistance or various maintence services on the site, we do provide maintenece service varying based on each specific case.
 				</div>
 			</div>
 			<div class="col-lg-4 text-center">
 				<img class="img-thumbnail rounded-circle d-flex align-center rounded mx-auto" src="riccardo-annandale-140624-unsplash.jpg" alt="">
 				<div class="card">
-					Digital Marketing & Social Media
+					Front-End & Back-End Development - We do provide services in both.
 				</div>
 			</div>
 		</div>
@@ -112,6 +113,8 @@
 				</div>
 			</div>
 		</div>
+
+
 
 
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
